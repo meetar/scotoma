c4dc2caaed86099aed6c5c11c14cd2c2505fd150
Peter Richardson <pxrich@gmail.com>
2025-11-23 11:50:09 -0800

    add build and deployment configs

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   optimizeDeps: {
     include: ['delaunator', 'd3-contour']
   }
